d5e03089b2f3b7be3f5ec9625fd8f6f166950cc0
minecrafterMc <78699603+minecrafterMc@users.noreply.github.com>
2024-05-26 18:40:24 +0200

    added map data

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 var map = L.map('map', {
     crs: L.CRS.Simple,
-    minZoom: 0
+    minZoom: 1,
+    zoomSnap: 0.25
 });
-var bounds = [[0,0], [1000,1000]];
-var image = L.imageOverlay('0_0.jpg', bounds).addTo(map);
-map.fitBounds(bounds);
-var marker = L.marker([1, 1]).addTo(map);
+var bounds = [[12,10], [1000,1000]];
+var image = L.tileLayer('https://www.zeldadungeon.net/maps/totk/tiles/surface/{z}/{x}_{y}.jpg', {maxZoom: 5}).addTo(map);
+//map.fitBounds(bounds);
+map.setView([50, 50], 0);
+var marker = L.marker([500, 500]).addTo(map);
 function onMapClick(e) {
     marker
-        .setLatLng(e.latlng)
-        .openOn(map);
+        .setLatLng(e.latlng);
+}
+function changeLevel(){
+
 }
 map.on('click', onMapClick);
+map.on("zoomend")
